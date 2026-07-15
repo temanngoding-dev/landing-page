3916c19a9b2de1b4e59ec77a88ea8c0c0d4ae008
temanngoding-dev <temanngoding.id@gmail.com>
2026-07-15 07:47:12 +0700

    chore: remove webpack polling configuration for Docker HMR

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Enable webpack polling so HMR works inside Docker
-  // (Docker volumes on Windows/Mac don't support inotify/FSEvents natively)
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,       // Check for changes every 1 second
-        aggregateTimeout: 300, // Delay rebuild slightly to batch changes
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
